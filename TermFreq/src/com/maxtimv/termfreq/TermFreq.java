154ec978910c29d93111ffd91dc95ead1597fb00
Maxim Timofeev <maxtimv@gmail.com>
2013-11-05 08:30:13 -0500

    First commit

---
+++ TermFreq/src/com/maxtimv/termfreq/TermFreq.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.maxtimv.termfreq;
+
+/**
+ * @author Maxim Timofeev
+ * 
+ */
+public class TermFreq {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("TermFreq!");
+	}
+}
