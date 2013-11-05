540cadbd6e3592242de3b38c7e28131818025218
Maxim Timofeev <maxtimv@gmail.com>
2013-11-05 12:06:35 -0500

    Implemented main method
    Signed-off-by: Maxim Timofeev <maxtimv@gmail.com>

--- TermFreq/src/com/maxtimv/termfreq/TermFreq.java
+++ TermFreq/src/com/maxtimv/termfreq/TermFreq.java
@@ -3,6 +3,13 @@
  */
 package com.maxtimv.termfreq;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * @author Maxim Timofeev
  * 
@@ -11,8 +18,78 @@ public class TermFreq {
 
 	/**
 	 * @param args
+	 *            the command line arguments
 	 */
-	public static void main(String[] args) {
-		System.out.println("TermFreq!");
+	public static void main(String[] args) throws IOException {
+		// Set defaults
+		ITermExtractor extractor = new TermExtractor();
+		Comparator<Term> comparator = null;
+		String fileName = null;
+
+		// Process arguments
+		for (String arg : args) {
+			if ("-acronym".equalsIgnoreCase(arg)
+					|| "-acr".equalsIgnoreCase(arg)) {
+				extractor = new TermExtractorAcronym();
+			} else if ("-sorta".equalsIgnoreCase(arg)
+					|| "-sa".equalsIgnoreCase(arg)) {
+				comparator = new TermComparatorA();
+			} else if ("-sortb".equalsIgnoreCase(arg)
+					|| "-sb".equalsIgnoreCase(arg)) {
+				comparator = new TermComparatorB();
+			} else {
+				fileName = arg;
+			}
+		}
+
+		// No input file?
+		if (fileName == null) {
+			usage();
+			return;
+		}
+
+		// Load the file and extract terms
+		String text = loadFile(fileName);
+		Term[] terms = extractor.extract(text);
+
+		// Sort the terms if asked
+		if (comparator != null) {
+			Arrays.sort(terms, comparator);
+		}
+
+		// Print results
+		System.out.println("Total terms: " + terms.length);
+		for (Term t : terms) {
+			System.out.println(t);
+		}
+	}
+
+	/**
+	 * Loads the content of the file into a String.
+	 */
+	private static String loadFile(String fileName) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				new FileInputStream(fileName)));
+		for (String line = reader.readLine(); line != null; line = reader
+				.readLine()) {
+			sb.append(line).append(' ');
+		}
+		reader.close();
+		return sb.toString();
+	}
+
+	/**
+	 * Prints usage instructions
+	 */
+	public static void usage() {
+		System.out.println("Usage: TermFreq [-options] inputFile");
+		System.out
+				.println("   (lists all terms found in <inputFile> with their frequency)");
+		System.out.println();
+		System.out.println("where options include:");
+		System.out.println("   -acronym | -acr    preserves acronyms");
+		System.out.println("   -sorta | -sa       uses sort method A");
+		System.out.println("   -sortb | -sb       uses sort method B");
 	}
 }
